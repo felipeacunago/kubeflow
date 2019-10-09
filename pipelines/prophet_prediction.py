7048645015ffecd407a4f118a778d72f9f5f9cc4
Felipe Acuña Gonzalez <facunag@sodimac.cl>
2019-10-09 17:55:40 -0300

    Prophet pipeline generalized

--- pipelines/prophet_prediction.py
+++ pipelines/prophet_prediction.py
@@ -16,10 +16,6 @@ ranker_op = comp.load_component_from_url(
     'https://raw.githubusercontent.com/membrilloski/kubeflow/master/ranker/component.yaml'
 )
 
-ranker_eval_op = comp.load_component_from_url(
-    'https://raw.githubusercontent.com/membrilloski/kubeflow/master/ranker-eval/component.yaml'
-)
-
 @dsl.pipeline(
   name='Prophet',
   description='A pipeline to train and serve the MNIST example.'
@@ -28,7 +24,7 @@ def prophet_pipeline(dataset_query='',
                    dictionary_query='',
                    val_dataset_query='',
                    minimum_length=10,
-                   training_date='2019-09-12',
+                   training_date='2019-10-05',
                    changepoint_prior_scale=0.01,
                    evaluation_date='',
                    evaluation_maximum_distance=1
@@ -41,17 +37,17 @@ def prophet_pipeline(dataset_query='',
         3. Generate a ranking based on prediction data and compare it to a ranking made with real data from that date
     """
 
-    rank_path_names=['']
-    ranking_factors=[]
-    project_id=''
-    split_column=''
-    gcs_root='gs://bucket/folder'
-    prediction_y='y'
-    predict_periods=10
-    dataset_location='US'
-    ds_column='ds'
-    predict_freq='D'
-    order_ds = 'asc'
+    rank_path_names=[''] # name of paths used for ranking (for example clicks)
+    ranking_factors=[] # factors used for ranking (score: a*x+b*y, ranking_factors=[a,b])
+    project_id='' # project name
+    split_column='' # column used to split the time series from the dataset
+    gcs_root='' # root path where all experiments files will be saved
+    prediction_y='' # y column name used for the timeseries
+    predict_periods=1 # number of periods used for timeseries prediction
+    dataset_location='US' # dataset location
+    ds_column='' # name of the column used for time
+    predict_freq='D' # prediction frequency (D: days)
+    order_ds = 'asc' # used to order items by date after splitting
 
     original_dataset_path='{}/input/dataset.csv'.format(gcs_root)
     val_dataset_path='{}/input/val_dataset.csv'.format(gcs_root)
@@ -62,123 +58,121 @@ def prophet_pipeline(dataset_query='',
     dictionary_file_path = '{}/dictionary/dictionary.csv'.format(gcs_root)
     prophet_rank_output_path= '{}/rankings/prediction/'.format(gcs_root)
     validation_rank_output='{}/rankings/validation/'.format(gcs_root)
+    results_output='{}/results/'.format(gcs_root)
 
     
 
-    # dataset_query_op = bigquery_query_op(
-    #     query=dataset_query, 
-    #     project_id=project_id,
-    #     output_gcs_path=original_dataset_path, 
-    #     dataset_location=dataset_location, 
-    #     job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
+    dataset_query_op = bigquery_query_op(
+        query=dataset_query, 
+        project_id=project_id,
+        output_gcs_path=original_dataset_path, 
+        dataset_location=dataset_location, 
+        job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
 
-    # preprocess = dsl.ContainerOp(
-    #     name='preprocess-split',
-    #     image='docker.io/felipeacunago/dataset-preprocess:latest',
-    #     arguments=[
-    #         "task.py",
-    #         "--dataset-path", dataset_query_op.outputs['output_gcs_path'],
-    #         "--output-path", preprocess_output_path,
-    #         "--split-column", split_column,
-    #         "--ds-column", ds_column,
-    #         "--y-column", prediction_y,
-    #         "--minimum-length", minimum_length,
-    #         "--order-ds", 'asc',
-    #         "--training-date", training_date
-    #         ]
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+    preprocess = dsl.ContainerOp(
+        name='preprocess-split',
+        image='docker.io/felipeacunago/dataset-preprocess:latest',
+        arguments=[
+            "task.py",
+            "--dataset-path", dataset_query_op.outputs['output_gcs_path'],
+            "--output-path", preprocess_output_path,
+            "--split-column", split_column,
+            "--ds-column", ds_column,
+            "--y-column", prediction_y,
+            "--minimum-length", minimum_length,
+            "--order-ds", 'asc',
+            "--training-date", training_date
+            ]
+    ).apply(gcp.use_gcp_secret('user-gcp-sa'))
 
-    # timeseries_prophet_train = dsl.ContainerOp(
-    #     name='prophet-train',
-    #     image='docker.io/felipeacunago/timeseries-prophet-train:latest',
-    #     arguments=[
-    #         "train.py",
-    #         "--dataset-path", preprocess_output_path,
-    #         "--changepoint-prior-scale", changepoint_prior_scale,
-    #         "--model-output-path", model_output_path
-    #         ]
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(preprocess)
+    timeseries_prophet_train = dsl.ContainerOp(
+        name='prophet-train',
+        image='docker.io/felipeacunago/timeseries-prophet-train:latest',
+        arguments=[
+            "train.py",
+            "--dataset-path", preprocess_output_path,
+            "--changepoint-prior-scale", changepoint_prior_scale,
+            "--model-output-path", model_output_path
+            ]
+    ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(preprocess)
 
-    # timeseries_prophet_predict = dsl.ContainerOp(
-    #     name='prophet-predict',
-    #     image='docker.io/felipeacunago/timeseries-prophet-predict:latest',
-    #     arguments=[
-    #         "predict.py",
-    #         "--predict-periods", predict_periods,
-    #         "--predict-freq", predict_freq,
-    #         "--model-path", model_output_path,
-    #         "--predictions-path", predictions_path
-    #         ]
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(timeseries_prophet_train)
+    timeseries_prophet_predict = dsl.ContainerOp(
+        name='prophet-predict',
+        image='docker.io/felipeacunago/timeseries-prophet-predict:latest',
+        arguments=[
+            "predict.py",
+            "--predict-periods", predict_periods,
+            "--predict-freq", predict_freq,
+            "--model-path", model_output_path,
+            "--predictions-path", predictions_path
+            ]
+    ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(timeseries_prophet_train)
 
-    # dataset_query_op = bigquery_query_op(
-    #     query=dictionary_query, 
-    #     project_id=project_id,
-    #     output_gcs_path=dictionary_file_path, 
-    #     dataset_location=dataset_location, 
-    #     job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
+    dataset_query_op = bigquery_query_op(
+        query=dictionary_query, 
+        project_id=project_id,
+        output_gcs_path=dictionary_file_path, 
+        dataset_location=dataset_location, 
+        job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
 
-    # prophet_rank = ranker_op(
-    #     input_path=predictions_path,
-    #     input_path_names=rank_path_names,
-    #     ranking_factors=ranking_factors,
-    #     input_dictionary=dataset_query_op.outputs['output_gcs_path'],
-    #     training_date=training_date,
-    #     ranking_output_path=prophet_rank_output_path,
-    #     prediction_periods=predict_periods
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(timeseries_prophet_predict)
+    prophet_rank = ranker_op(
+        input_path=predictions_path,
+        input_path_names=rank_path_names,
+        ranking_factors=ranking_factors,
+        input_dictionary=dataset_query_op.outputs['output_gcs_path'],
+        training_date=training_date,
+        ranking_output_path=prophet_rank_output_path,
+        prediction_periods=predict_periods
+    ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(timeseries_prophet_predict)
 
-    # val_query_op = bigquery_query_op(
-    #     query=val_dataset_query, 
-    #     project_id=project_id,
-    #     output_gcs_path=val_dataset_path, 
-    #     dataset_location=dataset_location, 
-    #     job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
+    val_query_op = bigquery_query_op(
+        query=val_dataset_query, 
+        project_id=project_id,
+        output_gcs_path=val_dataset_path, 
+        dataset_location=dataset_location, 
+        job_config='').apply(gcp.use_gcp_secret('user-gcp-sa'))
 
-    # val_preprocess = dsl.ContainerOp(
-    #     name='validation-preprocess-split',
-    #     image='docker.io/felipeacunago/dataset-preprocess:latest',
-    #     arguments=[
-    #         "task.py",
-    #         "--dataset-path", val_query_op.outputs['output_gcs_path'],
-    #         "--output-path", val_split_output_path,
-    #         "--split-column", split_column,
-    #         "--ds-column", ds_column,
-    #         "--y-column", prediction_y,
-    #         "--minimum-length", 1,
-    #         "--order-ds", order_ds,
-    #         "--training-date", evaluation_date
-    #         ]
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+    val_preprocess = dsl.ContainerOp(
+        name='validation-preprocess-split',
+        image='docker.io/felipeacunago/dataset-preprocess:latest',
+        arguments=[
+            "task.py",
+            "--dataset-path", val_query_op.outputs['output_gcs_path'],
+            "--output-path", val_split_output_path,
+            "--split-column", split_column,
+            "--ds-column", ds_column,
+            "--y-column", prediction_y,
+            "--minimum-length", 1,
+            "--order-ds", order_ds,
+            "--training-date", evaluation_date
+            ]
+    ).apply(gcp.use_gcp_secret('user-gcp-sa'))
 
-    # val_rank = ranker_op(
-    #     input_path=val_split_output_path,
-    #     input_path_names=rank_path_names,
-    #     ranking_factors=ranking_factors,
-    #     input_dictionary=dataset_query_op.outputs['output_gcs_path'],
-    #     training_date=evaluation_date,
-    #     ranking_output_path=validation_rank_output,
-    #     prediction_periods=predict_periods
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(val_preprocess)
+    val_rank = ranker_op(
+        input_path=val_split_output_path,
+        input_path_names=rank_path_names,
+        ranking_factors=ranking_factors,
+        input_dictionary=dataset_query_op.outputs['output_gcs_path'],
+        training_date=evaluation_date,
+        ranking_output_path=validation_rank_output,
+        prediction_periods=predict_periods
+    ).apply(gcp.use_gcp_secret('user-gcp-sa')).after(val_preprocess)
 
-    ranker_eval = ranker_eval_op(
-        predicted_ranking_path=prophet_rank_output_path, 
-        real_ranking_path=validation_rank_output,
-        eval_date=training_date, 
-        maximum_distance=evaluation_maximum_distance
-        ).apply(gcp.use_gcp_secret('user-gcp-sa'))
-
-    # rank_evaluation = dsl.ContainerOp(
-    #     name='rank-evaluation',
-    #     image='docker.io/felipeacunago/ranker-eval:latest',
-    #     arguments=[
-    #         "task.py",
-    #         "--predicted-ranking-path", prophet_rank_output_path,
-    #         "--real-ranking-path", validation_rank_output,
-    #         "--eval-date", training_date,
-    #         "--maximum-distance", evaluation_maximum_distance
-    #         ]
-    # ).apply(gcp.use_gcp_secret('user-gcp-sa'))
+    rank_evaluation = dsl.ContainerOp(
+        name='rank-evaluation',
+        image='docker.io/felipeacunago/ranker-eval:latest',
+        arguments=[
+            "task.py",
+            "--predicted-ranking-path", prophet_rank.outputs['ranking_output_path_file'],
+            "--real-ranking-path", val_rank.outputs['ranking_output_path_file'],
+            "--eval-date", training_date,
+            "--maximum-distance", evaluation_maximum_distance,
+            "--output", results_output,
+            ],
+        output_artifact_paths={
+            'mlpipeline-ui-metadata': '/mlpipeline-ui-metadata.json',
+        }
+    ).apply(gcp.use_gcp_secret('user-gcp-sa'))
 
 if __name__ == '__main__':
   import kfp.compiler as compiler
